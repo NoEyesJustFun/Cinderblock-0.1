6591997143c32309e8ab32bbb3645760fc97d35d
Bradley Urso <4131404+bradleyurso@users.noreply.github.com>
2020-03-02 09:45:06 -0800

    Add files via upload

---
+++ qmk/config.h
@@ -0,0 +1,37 @@
+/*
+Copyright 2019 NoEyesJustFun
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#include "config_common.h"
+
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x6061
+#define DEVICE_VER      0x0001
+#define MANUFACTURER 	NoEyesJustFun
+#define PRODUCT 		Cinderblock
+#define DESCRIPTION 	Equal Staggered Keyboard
+
+/* MATRIX SIZE */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 14
+
+#define DIODE_DIRECTION ROW2COL  
+
+/* WS2812 RGB LED */
+//#define RGB_DI_PIN D3
+//#define RGBLIGHT_ANIMATIONS
+//#define RGBLED_NUM 12               
